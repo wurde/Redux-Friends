e9840248a6abc5892d6e736bcb7e43efa3999e9f
Andy Bettisworth <andy@accreu.com>
2019-05-16 16:14:16 -0500

    Add PrivateRoute component

---
+++ friends/src/routes/helpers/PrivateRoute.js
@@ -0,0 +1,38 @@
+'use strict'
+
+/**
+ * Dependencies
+ */
+
+const React = require('react')
+const react_router_dom = require('react-router-dom')
+
+/**
+ * Constants
+ */
+
+const Component = React.Component
+const Redirect = react_router_dom.Redirect
+
+/**
+ * Define route component
+ */
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      localStorage.getItem("token") ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to="/users/login" />
+      )
+    }
+  />
+)
+
+/**
+ * Export route component
+ */
+
+module.exports = PrivateRoute
---
+++ friends/src/routes/helpers/index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+/**
+ * Export route helpers
+ */
+
+module.exports = {
+  PrivateRoute: require('./PrivateRoute'),
+}
